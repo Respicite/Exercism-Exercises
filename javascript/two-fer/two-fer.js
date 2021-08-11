90a41f6dda456b554493ad8dea673ee5bac14803
Respicite <Respicite@blueshard.net>
2021-08-11 08:38:47 +0200

    js - two-fer
    added better solution

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,1 +1,5 @@
-export const twoFer = (name) => `One for ${name ? name : 'you'}, one for me.`
+//Original solution
+export const twoFer_old = (name) => `One for ${name ? name : 'you'}, one for me.`
+
+//Solution after Mentor Feedback
+export const twoFer = (name='you') => `One for ${name}, one for me.`
